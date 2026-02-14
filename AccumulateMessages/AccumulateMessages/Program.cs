dc9898d8d55563b095b46f0c99fa635325f26db0
cambriajm <cambriajm@CAMDELL>
2026-02-14 13:11:33 -0700

    added title

--- AccumulateMessages/AccumulateMessages/Program.cs
+++ AccumulateMessages/AccumulateMessages/Program.cs
@@ -1,24 +1,50 @@
 ﻿namespace AccumulateMessages
+//Cambria Morgan
+//RCET 2265
+//Spring 2026
+//AccumulateMessage
+//https://github.com/cambriajm/ConvertAndValidate.git
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter your message, " +
+                "press q to quit and see your list");
 
-            Console.WriteLine(AccumulateMessage());
-            AccumulateMessage();
+                string userInput = "";
+                string list = "";
+                string message = list + userInput;
+            do
+            {
+                userInput = Console.ReadLine();
+                if(true)
+                {
+                    if(userInput == null)
+                        {
+                        
+                        Console.WriteLine(userInput);
+                         
+                        }
+                    else
+                        {
+                        list = list + "\n" + userInput;
+                        
+                        }
+                    message = list;
+                    
 
+                }
+                 
+                Console.WriteLine(message);
+                
+            } while (userInput != "Q" && userInput != "q");
+
+            Console.Clear();
+            Console.WriteLine($"The list of messages: \n{message}");
+            Console.WriteLine("Woah. ByeBye.");
             //pause
             Console.Read();
-        }
-    static string AccumulateMessage()
-        {
-            Console.WriteLine("Enter your message, press q to quit");
-            string userInput = "";
-            Console.ReadLine();
-            return (userInput) ;
-        //pause
-        Console.Read();
         }
     }
 }
